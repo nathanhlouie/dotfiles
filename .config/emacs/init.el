2a5cb73c009e37da79bd4db853e99e9655e97266
Nathan Louie <nathanhlouie@gmail.com>
2025-01-14 10:28:24 -0500

    use elpaca package manager, clean config for now

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,106 +1,41 @@
-(setq gc-cons-threshold #x40000000)
-(setq read-process-output-max (* 1024 1024 4))
+(defvar elpaca-installer-version 0.8)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(use-package emacs
-  :ensure nil
-  :custom
-  (auto-save-default nil)
-  (column-number-mode t)
-  (create-lockfiles nil)
-  (delete-by-moving-to-trash t)
-  (delete-selection-mode 1)
-  (display-line-numbers-type t)
-  (global-auto-revert-non-file-buffers t)
-  (history-length 25)
-  (inhibit-startup-message t)
-  (initial-scratch-message "")
-  (ispell-dictionary "en_GB")
-  (make-backup-files nil)
-  (pixel-scroll-precision-mode t)
-  (pixel-scroll-precision-use-momentum nil)
-  (ring-bell-function 'ignore)
-  (split-width-threshold 300)
-  (switch-to-buffer-obey-display-actions t)
-  (tab-always-indent 'complete)
-  (tab-width 4)
-  (treesit-font-lock-level 4)
-  (truncate-lines t)
-  (use-dialog-box nil)
-  (use-short-answers t)
-  (warning-minimum-level :emergency)
-
-  :hook
-  (prog-mode . display-line-numbers-mode)
-
-  :config
-  (defun skip-these-buffers (_window buffer _bury-or-kill)
-	"Function for `switch-to-prev-buffer-skip'."
-	(string-match "\\*[^*]+\\*" (buffer-name buffer)))
-  (setq switch-to-prev-buffer-skip 'skip-these-buffers)
-
-  (set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 240 :weight 'medium)
-  (setq mac-command-modifier 'meta)
-
-  (set-display-table-slot standard-display-table 'vertical-border (make-glyph-code ?│))
-
-  :init
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-
-  (when scroll-bar-mode
-	(scroll-bar-mode -1))
-
-  (global-hl-line-mode 1)
-  (global-auto-revert-mode 1)
-  (indent-tabs-mode -1)
-  (recentf-mode 1)
-  (savehist-mode 1)
-  (save-place-mode 1)
-  (winner-mode 1)
-  (xterm-mouse-mode 1)
-  (file-name-shadow-mode 1)
-
-  (modify-coding-system-alist 'file "" 'utf-8))
-
-(use-package window
-  :ensure nil
-  :custom
-  (display-buffer-alist
-   '(
-	 ("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|[Hh]elp\\|Messages\\|Bookmark List\\|Ibuffer\\|Occur\\|eldoc.*\\)\\*"
-      (display-buffer-in-side-window)
-      (window-height . 0.25)
-      (side . bottom)
-      (slot . 0))
-
-     ("\\*\\(lsp-help\\)\\*"
-      (display-buffer-in-side-window)
-      (window-height . 0.25)
-      (side . bottom)
-      (slot . 0))
-     
-     ("\\*\\(Flymake diagnostics\\|xref\\|ivy\\|Swiper\\|Completions\\)"
-      (display-buffer-in-side-window)
-      (window-height . 0.25)
-      (side . bottom)
-      (slot . 1))
-	 )))
-
-(use-package dired
-  :ensure nil
-  :custom
-  (dired-listing-switches "-lah --group-directories-first")
-  (dired-dwim-target t)
-  (dired-guess-shell-alist-user
-   '(("\\.\\(png\\|jpe?g\\|tiff\\)" "feh" "xdg-open" "open")
-     ("\\.\\(mp[34]\\|m4a\\|ogg\\|flac\\|webm\\|mkv\\)" "mpv" "xdg-open" "open")
-     (".*" "open" "xdg-open")))
-  (dired-kill-when-opening-new-dired-buffer t)
-  :config
-  (when (eq system-type 'darwin)
-    (let ((gls (executable-find "gls")))
-      (when gls
-        (setq insert-directory-program gls)))))
+(elpaca elpaca-use-package
+		(elpaca-use-package-mode))
