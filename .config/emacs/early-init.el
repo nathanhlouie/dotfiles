6c44760dd5b714b24bb40135e94b98af08a1a8ea
Nathan Louie <nathanhlouie@gmail.com>
2025-01-14 11:06:50 -0500

    changes to early-init

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,8 +1,25 @@
-(setq
- site-run-file nil
- inhibit-default-init t
- gc-cons-threshold most-positive-fixnum
- package-enable-at-startup nil)
+(setq site-run-file nil
+      inhibit-default-init t
+      gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5
+      package-enable-at-startup nil)
+
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t
+      frame-title-format '("%b")
+      ring-bell-function 'ignore
+      use-dialog-box t
+      use-file-dialog nil
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-startup-buffer-menu t)
+
+(menu-bar-mode 1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 
 (add-hook 'after-init-hook
-		  #'(lambda () (setq gc-cons-threshold (* 8 1024 1024))))
+	  #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
+			     gc-cons-percentage 0.1)))
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -38,4 +38,6 @@
 (elpaca `(,@elpaca-order))
 
 (elpaca elpaca-use-package
-		(elpaca-use-package-mode))
+  (elpaca-use-package-mode))
+
+
