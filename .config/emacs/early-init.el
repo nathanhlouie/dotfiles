6c6fb5a66cce6499b8f025c407e2e4b0d89c0b95
Nathan Louie <nathanhlouie@gmail.com>
2025-01-16 11:16:44 -0500

    tangle early-init

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,9 +1,11 @@
-(setq site-run-file nil
-      inhibit-default-init t
-      gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.5
-      package-enable-at-startup nil)
+(setq site-run-file nil)
+(setq inhibit-default-init t)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5)
+
+(setq package-enable-at-startup nil)
 
 (add-hook 'after-init-hook
-	  #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
-			     gc-cons-percentage 0.1)))
+          #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
+                             gc-cons-percentage 0.1)))
