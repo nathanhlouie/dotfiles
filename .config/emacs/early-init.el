afddee69f77f1f5560de1f9d41e72da250aa2da8
Nathan Louie <nathanhlouie@gmail.com>
2025-01-17 17:07:12 -0500

    add minad stack

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,10 +1,10 @@
 (setq package-enable-at-startup nil)
 
 (setq site-run-file nil
-	  inhibit-default-init t
-	  gc-cons-threshold most-positive-fixnum
-	  gc-cons-percentage 0.5)
+	      inhibit-default-init t
+	      gc-cons-threshold most-positive-fixnum
+	      gc-cons-percentage 0.5)
 
 (add-hook 'after-init-hook
-		  #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
+		      #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
 							 gc-cons-percentage 0.1)))
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,34 +5,34 @@
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-							  :ref nil :depth 1
-							  :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-							  :build (:not elpaca--activate-package)))
+							      :ref nil :depth 1
+							      :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+							      :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-	   (build (expand-file-name "elpaca/" elpaca-builds-directory))
-	   (order (cdr elpaca-order))
-	   (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
+	       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+	       (order (cdr elpaca-order))
+	       (default-directory repo))
+      (add-to-list 'load-path (if (file-exists-p build) build repo))
+      (unless (file-exists-p repo)
 	(make-directory repo t)
 	(when (< emacs-major-version 28) (require 'subr-x))
 	(condition-case-unless-debug err
 		(if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-				  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-												  ,@(when-let* ((depth (plist-get order :depth)))
-													  (list (format "--depth=%d" depth) "--no-single-branch"))
-												  ,(plist-get order :repo) ,repo))))
-				  ((zerop (call-process "git" nil buffer t "checkout"
+				      ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+												      ,@(when-let* ((depth (plist-get order :depth)))
+													      (list (format "--depth=%d" depth) "--no-single-branch"))
+												      ,(plist-get order :repo) ,repo))))
+				      ((zerop (call-process "git" nil buffer t "checkout"
 										(or (plist-get order :ref) "--"))))
-				  (emacs (concat invocation-directory invocation-name))
-				  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+				      (emacs (concat invocation-directory invocation-name))
+				      ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
 										"--eval" "(byte-recompile-directory \".\" 0 'force)")))
-				  ((require 'elpaca))
-				  ((elpaca-generate-autoloads "elpaca" repo)))
+				      ((require 'elpaca))
+				      ((elpaca-generate-autoloads "elpaca" repo)))
 			(progn (message "%s" (buffer-string)) (kill-buffer buffer))
-		  (error "%s" (with-current-buffer buffer (buffer-string))))
-	  ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
+		      (error "%s" (with-current-buffer buffer (buffer-string))))
+	      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+      (unless (require 'elpaca-autoloads nil t)
 	(require 'elpaca)
 	(elpaca-generate-autoloads "elpaca" repo)
 	(load "./elpaca-autoloads")))
@@ -40,45 +40,49 @@
 (elpaca `(,@elpaca-order))
 
 (elpaca elpaca-use-package
-  (elpaca-use-package-mode))
+      (elpaca-use-package-mode))
 
 (setq frame-inhibit-implied-resize t
-	  frame-resize-pixelwise t
-	  frame-title-format '("%b")
-	  ring-bell-function 'ignore
-	  split-width-threshold 300
-	  visible-bell nil)
+	      frame-resize-pixelwise t
+	      frame-title-format '("%b")
+	      ring-bell-function 'ignore
+	      split-width-threshold 300
+	      visible-bell nil)
 
 (setq pixel-scroll-precision-mode t
-	  pixel-scroll-precision-use-momentum nil)
+	      pixel-scroll-precision-use-momentum nil)
 
 (setq inhibit-splash-screen t
-	  inhibit-startup-buffer-menu t
-	  inhibit-startup-echo-area-message user-login-name
-	  inhibit-startup-message t
-	  inhibit-startup-screen t
-	  initial-buffer-choice t
-	  initial-scratch-message "")
+	      inhibit-startup-buffer-menu t
+	      inhibit-startup-echo-area-message user-login-name
+	      inhibit-startup-message t
+	      inhibit-startup-screen t
+	      initial-buffer-choice t
+	      initial-scratch-message "")
 
-(setq pop-up-windows nil
-	  use-dialog-box nil
-	  use-file-dialog nil
-	  use-short-answers t
-	  show-help-function nil
-	  warning-minimum-level :emergency)
+(setq cursor-in-non-selected-windows nil
+      indicate-empty-lines nil
+      pop-up-windows nil
+      use-dialog-box nil
+      use-file-dialog nil
+      use-short-answers t
+      show-help-function nil
+      warning-minimum-level :emergency)
 
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (scroll-bar-mode -1)
 
-(menu-bar-mode 1)
+(if (display-graphic-p)
+    (menu-bar-mode 1)
+  (menu-bar-mode -1))
 
 (setq create-lockfiles nil
-	  make-backup-files nil)
+	      make-backup-files nil)
 
 (setq auto-save-default t
-	  auto-save-interval 200
-	  auto-save-timeout 20)
+	      auto-save-interval 200
+	      auto-save-timeout 20)
 
 (setq delete-by-moving-to-trash t)
 
@@ -89,6 +93,26 @@
 (set-default-coding-systems 'utf-8)
 (setq ispell-dictionary "en_US")
 
+(when (eq system-type 'darwin)
+  (setq ns-use-native-fullscreen t
+	mac-option-key-is-meta nil
+	mac-command-key-is-meta t
+	mac-command-modifier 'meta
+	mac-option-modifier nil
+	mac-use-title-bar nil))
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(when (and (not (display-graphic-p))
+	   (eq system-type 'darwin))
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
 (global-auto-revert-mode t)
 (global-auto-revert-non-file-buffers t)
 
@@ -97,3 +121,60 @@
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 
 (setq-default indent-tabs-mode nil)
+
+(global-hl-line-mode 1)
+
+(recentf-mode 1)
+(savehist-mode 1)
+(save-place-mode 1)
+(winner-mode 1)
+(xterm-mouse-mode 1)
+
+(setq enable-recursive-minibuffers t)
+
+(use-package vertico
+  :ensure t
+  :hook
+  (after-init . vertico-mode)
+  :custom
+  (vertico-count 10)
+  (vertico-resize nil)
+  (advice-add #'vertico--format-candidate :around
+	      (lambda (orig cand prefix suffix index _start)
+		(setq cand (funcall orig cand prefix suffix index _start))
+		(concat
+		 (if (= vertico--index index)
+		     (propertize "» " 'face '(:foreground "#80adf0" :weight bold))
+		   "  ")
+		 cand))))
+
+(use-package orderless
+  :ensure t
+  :defer t
+  :after vertico
+  :config
+  (setq completion-styles '(orderless basic)
+	completion-category-defaults nil
+	completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package marginalia
+  :ensure t
+  :hook
+  (after-init . marginalia-mode))
+
+(use-package consult
+  :ensure t
+  :defer t
+  :config
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq xref-show-xrefs-function #'consult-xref
+	xref-show-definitions-function #'consult-xref))
+
+(use-package embark
+  :ensure t
+  :defer t)
+
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
