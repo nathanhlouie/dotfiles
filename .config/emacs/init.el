bd16bf56c0e3385b8453e9e644228cbe914d5b51
Nathan Louie <nathanhlouie@gmail.com>
2025-01-17 21:45:04 -0500

    use correct recipe keywords for elpaca

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,46 +1,51 @@
 ;;; init.el -*- lexical-binding: t -*-
 
+;;; init.el -*- lexical-binding: t -*-
+
 (defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-							      :ref nil :depth 1
-							      :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-							      :build (:not elpaca--activate-package)))
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-	       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-	       (order (cdr elpaca-order))
-	       (default-directory repo))
-      (add-to-list 'load-path (if (file-exists-p build) build repo))
-      (unless (file-exists-p repo)
-	(make-directory repo t)
-	(when (< emacs-major-version 28) (require 'subr-x))
-	(condition-case-unless-debug err
-		(if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-				      ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-												      ,@(when-let* ((depth (plist-get order :depth)))
-													      (list (format "--depth=%d" depth) "--no-single-branch"))
-												      ,(plist-get order :repo) ,repo))))
-				      ((zerop (call-process "git" nil buffer t "checkout"
-										(or (plist-get order :ref) "--"))))
-				      (emacs (concat invocation-directory invocation-name))
-				      ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-										"--eval" "(byte-recompile-directory \".\" 0 'force)")))
-				      ((require 'elpaca))
-				      ((elpaca-generate-autoloads "elpaca" repo)))
-			(progn (message "%s" (buffer-string)) (kill-buffer buffer))
-		      (error "%s" (with-current-buffer buffer (buffer-string))))
-	      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-      (unless (require 'elpaca-autoloads nil t)
-	(require 'elpaca)
-	(elpaca-generate-autoloads "elpaca" repo)
-	(load "./elpaca-autoloads")))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
 (elpaca elpaca-use-package
-      (elpaca-use-package-mode))
+  (elpaca-use-package-mode)
+  (setq use-package-always-ensure t))
+
+(elpaca-wait)
 
 (setq frame-inhibit-implied-resize t
 	      frame-resize-pixelwise t
@@ -114,7 +119,6 @@
   (setq interprogram-paste-function 'copy-from-osx))
 
 (global-auto-revert-mode t)
-(global-auto-revert-non-file-buffers t)
 
 (setq sentence-end-double-space nil)
 
@@ -130,51 +134,42 @@
 (winner-mode 1)
 (xterm-mouse-mode 1)
 
-(setq enable-recursive-minibuffers t)
-
 (use-package vertico
-  :ensure t
-  :hook
-  (after-init . vertico-mode)
+  :demand t
   :custom
+  (enable-recursive-minibuffers t)
   (vertico-count 10)
   (vertico-resize nil)
-  (advice-add #'vertico--format-candidate :around
-	      (lambda (orig cand prefix suffix index _start)
-		(setq cand (funcall orig cand prefix suffix index _start))
-		(concat
-		 (if (= vertico--index index)
-		     (propertize "» " 'face '(:foreground "#80adf0" :weight bold))
-		   "  ")
-		 cand))))
+  (vertico-cycle t)
+  :config
+  (vertico-mode))
 
 (use-package orderless
-  :ensure t
-  :defer t
-  :after vertico
-  :config
-  (setq completion-styles '(orderless basic)
-	completion-category-defaults nil
-	completion-category-overrides '((file (styles partial-completion)))))
+  :defer 1
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package marginalia
-  :ensure t
-  :hook
-  (after-init . marginalia-mode))
+  :defer 2
+  :config
+  (marginalia-mode))
 
 (use-package consult
-  :ensure t
-  :defer t
+  :demand t
   :config
   (advice-add #'register-preview :override #'consult-register-window)
   (setq xref-show-xrefs-function #'consult-xref
 	xref-show-definitions-function #'consult-xref))
 
 (use-package embark
-  :ensure t
-  :defer t)
+  :demand t)
 
 (use-package embark-consult
-  :ensure t
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
+
+(setq treesit-font-lock-level 4)
+
+(set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 240 :weight 'medium)
