d49d21f986767868287946d318d1ebf9a2a775f5
Nathan Louie <nathanhlouie@gmail.com>
2025-02-05 21:50:42 -0500

    revert back to basics

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el -*- lexical-binding: t -*-
+;;; early-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
 
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,4 @@
-;;; init.el -*- lexical-binding: t -*-
+;;; init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
 (defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
@@ -46,22 +46,22 @@
 (elpaca-wait)
 
 (setq frame-inhibit-implied-resize t
-      	frame-resize-pixelwise t
-      	frame-title-format '("%b")
-      	ring-bell-function 'ignore
-      	split-width-threshold 300
-      	visible-bell nil)
+	      frame-resize-pixelwise t
+	      frame-title-format '("%b")
+	      ring-bell-function 'ignore
+	      split-width-threshold 300
+	      visible-bell nil)
 
 (setq pixel-scroll-precision-mode t
-      	pixel-scroll-precision-use-momentum nil)
+	      pixel-scroll-precision-use-momentum nil)
 
 (setq inhibit-splash-screen t
-      	inhibit-startup-buffer-menu t
-      	inhibit-startup-echo-area-message user-login-name
-      	inhibit-startup-message t
-      	inhibit-startup-screen t
-      	initial-buffer-choice t
-      	initial-scratch-message "")
+	      inhibit-startup-buffer-menu t
+	      inhibit-startup-echo-area-message user-login-name
+	      inhibit-startup-message t
+	      inhibit-startup-screen t
+	      initial-buffer-choice t
+	      initial-scratch-message "")
 
 (setq cursor-in-non-selected-windows nil
       indicate-empty-lines nil
@@ -102,11 +102,11 @@
 
 (when (eq system-type 'darwin)
   (setq ns-use-native-fullscreen t
-        mac-option-key-is-meta nil
-        mac-command-key-is-meta t
-        mac-command-modifier 'meta
-        mac-option-modifier nil
-        mac-use-title-bar nil))
+	mac-option-key-is-meta nil
+	mac-command-key-is-meta t
+	mac-command-modifier 'meta
+	mac-option-modifier nil
+	mac-use-title-bar nil))
 
 (defun copy-from-osx ()
   (shell-command-to-string "pbpaste"))
@@ -116,7 +116,7 @@
       (process-send-string proc text)
       (process-send-eof proc))))
 (when (and (not (display-graphic-p))
-           (eq system-type 'darwin))
+	   (eq system-type 'darwin))
   (setq interprogram-cut-function 'paste-to-osx)
   (setq interprogram-paste-function 'copy-from-osx))
 
@@ -160,192 +160,6 @@
   :ensure (:wait t)
   :defer t)
 
-(use-package vertico
-  :demand t
-  :bind (:map vertico-map ("C-q" . #'vertico-quick-insert))
-  :custom
-  (enable-recursive-minibuffers t)
-  (read-extended-command-predicate #'command-completion-default-include-p)
-  (read-file-name-completion-ignore-case t)
-  (read-buffer-completion-ignore-case t)
-  (completion-ignore-case t)
-  (vertico-scroll-margin 0)
-  (vertico-count 10)
-  (vertico-resize t)
-  (vertico-cycle t)
-  (vertico-multiform-commands '((consult-line buffer)
-                                (consult-imenu reverse buffer)))
-  (vertico-multiform-categories '((embark-keybinding grid)
-                                  (file flat)))
-  :config
-  (vertico-mode)
-  (vertico-multiform-mode)
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-  (defun +embark-live-vertico ()
-    "Shrink Vertico minibuffer when `embark-live' is active."
-    (when-let (win (and (string-prefix-p "*Embark Live" (buffer-name))
-                        (active-minibuffer-window)))
-      (with-selected-window win
-        (when (and (bound-and-true-p vertico--input)
-                   (fboundp 'vertico-multiform-unobtrusive))
-          (vertico-multiform-unobtrusive)))))
-  (add-hook 'embark-collect-mode-hook #'+embark-live-vertico))
-
-(use-package orderless
-  :defer 1
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion)))))
-
-(use-package marginalia
-  :defer 2
-  :config
-  (marginalia-mode))
-
-(use-package consult
-  :demand t
-  :bind (("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
-         ([remap Info-search] . consult-info)
-         ("C-x M-:" . consult-complex-command)
-         ("C-x b" . consult-buffer)
-         ("C-x 4 b" . consult-buffer-other-window)
-         ("C-x 5 b" . consult-buffer-other-frame)
-         ("C-x t b" . consult-buffer-other-tab)
-         ("C-x r b" . consult-bookmark)
-         ("C-x p b" . consult-project-buffer)
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)
-         ("C-M-#" . consult-register)
-         ("M-y" . consult-yank-pop)
-         ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)
-         ("M-g g" . consult-goto-line)
-         ("M-g M-g" . consult-goto-line)
-         ("M-g o" . consult-outline)
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ("M-s d" . consult-find)
-         ("M-s c" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)
-         ("M-s e" . consult-isearch-history)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         :map minibuffer-local-map
-         ("M-s" . consult-history)
-         ("M-r" . consult-history))
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-  :custom
-  (register-preview-delay 0.5)
-  (xref-show-xrefs-function #'consult-xref)
-  (xref-show-definitions-function #'consult-xref)
-  (consult-narrow-key "<")
-  (consult-preview-key 'any)
-  (consult-line-numbers-widen t)
-  :config
-  (advice-add #'register-preview :override #'consult-register-window)
-  (consult-customize
-   consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep consult-man
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
-   :preview-key '(:debounce 0.4 any)))
-
-(use-package embark
-  :demand t
-  :bind (("C-." . embark-act)
-         ("M-." . embark-dwim)
-         ("C-h B" . embark-bindings)
-         :map minibuffer-local-map
-         ("C-c C-c" . embark-collect)
-         ("C-c C-e" . embark-export))
-  :config
-  (setq prefix-help-command #'embark-prefix-help-command)
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
-
-(use-package embark-consult
-  :defer 1
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
-
-(use-package wgrep
-  :defer 1)
-
-(use-package cape
-  :demand t
-  :bind ("C-c p" . cape-prefix-map)
-  :config
-  (add-hook 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-file)
-  (add-hook 'completion-at-point-functions #'cape-line)
-  (add-hook 'completion-at-point-functions #'cape-elisp-block)
-  (add-hook 'completion-at-point-functions #'cape-history)
-  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster))
-
-(use-package avy
-  :demand t
-  :bind (("C-;" . avy-resume)
-         ("M-g c" . avy-goto-char)
-         ("M-g f" . avy-goto-line)
-         ("M-g w" . avy-goto-word-1))
-  :config
-  (avy-setup-default))
-
-(use-package anzu
-  :defer 10
-  :config (global-anzu-mode))
-
-(use-package corfu
-  :demand t
-  :custom
-  (corfu-quit-no-match t)
-  (global-corfu-minibuffer
-   (lambda ()
-     (not (or (bound-and-true-p mct--active)
-              (bound-and-true-p vertico--input)
-              (eq (current-local-map) read-passwd-map)))))
-  (corfu-cycle t)
-  (corfu-preselect 'prompt)
-  (completion-category-overrides '((eglot (styles orderless))
-                                   (eglot-capf (styles orderless))))
-  :config
-  (keymap-unset corfu-map "RET")
-  (global-corfu-mode))
-
-(use-package corfu-candidate-overlay
-  :demand t
-  ;;:bind (("<tab>" . completion-at-point)
-  ;;       ("C-<tab>" . corfu-candidate-overlay-complete-at-point))
-  :config
-  (corfu-candidate-overlay-mode))
-
-(use-package projectile
-  :demand t
-  :config
-  (add-to-list 'projectile-globally-ignored-directories "*node_modules")
-  (projectile-mode))
-
 (use-package flymake
   :config
   (flymake-mode))
@@ -355,18 +169,9 @@
   :config
   (add-hook 'eglot-server-initialized-hook #'flymake-mode))
 
-(use-package apheleia
-  :config
-  (apheleia-global-mode))
-
 (use-package treesit-auto
   :config
   (global-treesit-auto-mode))
-
-(use-package dape
-  :config
-  (dape-breakpoint-global-mode)
-  (repeat-mode))
 
 (use-package vterm)
 
@@ -385,16 +190,6 @@
 (use-package forge
   :after magit)
 
-(use-package diff-hl
-  :defer t
-  :config
-  (global-diff-hl-mode))
-
-(use-package smartparens
-  :config
-  (require 'smartparens-config)
-  (smartparens-global-mode))
-
 (setq dired-mouse-drag-files t)
 (setq dired-listing-switches "-alh")
 (setq dired-kill-when-opening-new-dired-buffer t)
@@ -402,19 +197,5 @@
 (setq treesit-font-lock-level 4)
 
 (set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 240 :weight 'medium)
-
-(use-package doom-modeline
-  :defer 2
-  :custom
-  (doom-modeline-time-analogue-clock nil)
-  (doom-modeline-time-icon nil)
-  (doom-modeline-unicode-fallback nil)
-  (doom-modeline-buffer-encoding 'nondefault)
-  (doom-modeline-icon t)
-  :config
-  (doom-modeline-mode))
-
-(use-package org
-  :defer t)
 
 (use-package which-key)
