00afa067a0d0d411edf857c40a4597124de35784
Nathan Louie <nathanhlouie@gmail.com>
2025-02-06 14:35:42 -0500

    minimal init

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,16 +1,143 @@
 ;;; early-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
-(setq package-enable-at-startup nil)
-
-(setq site-run-file nil
-      inhibit-default-init t
-      gc-cons-threshold most-positive-fixnum
+(setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
 
-(add-hook 'after-init-hook
-          #'(lambda () (setq gc-cons-threshold (* 8 1024 1024)
+(add-hook 'emacs-startup-hook
+          #'(lambda () (setq gc-cons-threshold (* 16 1024 1024)
                              gc-cons-percentage 0.1)))
 
-(when (native-comp-available-p)
-  (setq native-comp-async-report-warnings-errors 'silent)
-  (setq native-compile-prune-cache t))
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
+
+(setq load-prefer-newer t)
+(setq inhibit-compacting-font-caches t)
+
+(unless (daemonp)
+  (let ((old-value (default-toplevel-value 'file-name-handler-alist)))
+    (set-default-toplevel-value 'file-name-handler-alist
+                                (if (eval-when-compile
+                                      (locate-file-internal "calc-loaddefs.el"
+                                                            load-path))
+                                    nil
+                                  (list (rassq 'jka-compr-handler old-value))))
+    (put 'file-name-handler-alist 'initial-value old-value)
+    (define-advice command-line-1 (:around (fn args-left) respect-file-handlers)
+      (let ((file-name-handler-alist
+             (if args-left old-value file-name-handler-alist)))
+        (funcall fn args-left)))
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (set-default-toplevel-value
+                 'file-name-handler-alist
+                 (delete-dups (append file-name-handler-alist old-value))))
+              101))
+
+  (unless noninteractive
+
+    (setq-default inhibit-message t)
+
+    (defun minimal-emacs--reset-inhibited-vars-h ()
+      (setq-default inhibit-message nil)
+      (remove-hook 'post-command-hook #'minimal-emacs--reset-inhibited-vars-h))
+
+    (add-hook 'post-command-hook
+              #'minimal-emacs--reset-inhibited-vars-h -100)
+
+    (put 'mode-line-format 'initial-value
+         (default-toplevel-value 'mode-line-format))
+    (setq-default mode-line-format nil)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (setq mode-line-format nil)))
+
+    (defun minimal-emacs--startup-load-user-init-file (fn &rest args)
+      "Advice for startup--load-user-init-file to reset mode-line-format."
+      (unwind-protect
+          (apply fn args)
+        (setq-default inhibit-message nil)
+        (unless (default-toplevel-value 'mode-line-format)
+          (setq-default mode-line-format
+                        (get 'mode-line-format 'initial-value)))))
+
+    (advice-add 'startup--load-user-init-file :around
+                #'minimal-emacs--startup-load-user-init-file))
+
+  (setq frame-inhibit-implied-resize t)
+  (setq auto-mode-case-fold nil)
+
+  (setq inhibit-startup-screen t
+        inhibit-startup-message t
+        inhibit-startup-echo-area-message user-login-name)
+  (setq initial-buffer-choice nil
+        inhibit-startup-buffer-menu t
+        inhibit-x-resources t)
+
+  (setq-default bidi-display-reordering 'left-to-right
+                bidi-paragraph-direction 'left-to-right)
+
+  (setq bidi-inhibit-bpa t)
+
+  (advice-add #'display-startup-echo-area-message :override #'ignore)
+
+  (advice-add #'display-startup-screen :override #'ignore)
+
+  (setq initial-major-mode 'fundamental-mode
+        initial-scratch-message nil)
+
+  (unless (eq system-type 'darwin)
+    (setq command-line-ns-option-alist nil))
+  (unless (memq initial-window-system '(x pgtk))
+    (setq command-line-x-option-alist nil)))
+
+(if (and (featurep 'native-compile)
+         (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (setq native-comp-jit-compilation t
+          package-native-compile t)
+  (setq features (delq 'native-compile features)))
+
+(setq native-comp-async-report-warnings-errors 'silent)
+(setq native-comp-warning-on-missing-source nil)
+
+(setq debug-on-error nil
+      jka-compr-verbose nil)
+
+(setq byte-compile-warnings nil)
+(setq byte-compile-verbose nil)
+
+(setq frame-title-format "%b"
+      icon-title-format "%b")
+
+(setq inhibit-splash-screen t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(unless (memq window-system '(mac ns))
+  (setq menu-bar-mode nil))
+
+(unless (daemonp)
+  (unless noninteractive
+    (when (fboundp 'tool-bar-setup)
+      (advice-add #'tool-bar-setup :override #'ignore)
+      (define-advice startup--load-user-init-file
+          (:after (&rest _) minimal-emacs-setup-toolbar)
+        (advice-remove #'tool-bar-setup #'ignore)
+        (when tool-bar-mode
+          (tool-bar-setup))))))
+(push '(tool-bar-lines . 0) default-frame-alist)
+(setq tool-bar-mode nil)
+
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(horizontal-scroll-bars) default-frame-alist)
+(setq scroll-bar-mode nil)
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+(when (bound-and-true-p tooltip-mode)
+  (tooltip-mode -1))
+
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+
+(setq package-enable-at-startup nil
+      use-package-always-ensure t)
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -40,48 +40,78 @@
 (elpaca `(,@elpaca-order))
 
 (elpaca elpaca-use-package
-  (elpaca-use-package-mode)
-  (setq use-package-always-ensure t))
+  (elpaca-use-package-mode))
 
 (elpaca-wait)
 
-(setq frame-inhibit-implied-resize t
-	      frame-resize-pixelwise t
-	      frame-title-format '("%b")
-	      ring-bell-function 'ignore
-	      split-width-threshold 300
-	      visible-bell nil)
+(setq ffap-machine-p-known 'reject)
 
-(setq pixel-scroll-precision-mode t
-	      pixel-scroll-precision-use-momentum nil)
+(setq ad-redefinition-action 'accept)
+(setq warning-suppress-types '((lexical-binding)))
+(setq warning-minimum-level :error)
 
-(setq inhibit-splash-screen t
-	      inhibit-startup-buffer-menu t
-	      inhibit-startup-echo-area-message user-login-name
-	      inhibit-startup-message t
-	      inhibit-startup-screen t
-	      initial-buffer-choice t
-	      initial-scratch-message "")
+(setq enable-recursive-minibuffers t)
 
-(setq cursor-in-non-selected-windows nil
-      indicate-empty-lines nil
-      use-dialog-box nil
-      use-file-dialog nil
-      use-short-answers t
-      show-help-function nil)
+(setq minibuffer-prompt-properties
+      '(read-only t intangible t cursor-intangible t face
+                  minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(scroll-bar-mode -1)
+(setq idle-update-delay 1.0)
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (advice-add #'yes-or-no-p :override #'y-or-n-p))
+(defalias #'view-hello-file #'ignore)
 
-(if (display-graphic-p)
-    (menu-bar-mode 1)
-  (menu-bar-mode -1))
+(setq show-paren-delay 0.1
+      show-paren-highlight-openparen t
+      show-paren-when-point-inside-paren t
+      show-paren-when-point-in-periphery t)
+(show-paren-mode +1)
+
+(setq compilation-always-kill t
+      compilation-ask-about-save nil
+      compilation-scroll-output 'first-error)
+
+(setq whitespace-line-column nil)
+
+(setq rainbow-delimiters-max-face-count 5)
+
+(setq-default display-line-numbers-width 3)
+(setq-default display-line-numbers-widen t)
+
+(setq truncate-string-ellipsis "…")
+
+(setq read-process-output-max (* 512 1024))
+
+(setq redisplay-skip-fontification-on-input t)
+
+(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
+
+(setq abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
+
+(global-hl-line-mode 1)
+(winner-mode 1)
+(delete-selection-mode 1)
+
+(setq delete-by-moving-to-trash (not noninteractive))
+(setq find-file-suppress-same-file-warnings t)
+
+(setq find-file-visit-truename t
+      vc-follow-symlinks t)
+
+(setq split-width-threshold 170
+      split-height-threshold nil)
+
+(setq window-divider-default-bottom-width 1
+      window-divider-default-places t
+      window-divider-default-right-width 1)
 
 (setq create-lockfiles nil
       make-backup-files nil)
 
 (setq auto-save-default t
+      auto-save-include-big-deletions t
       auto-save-interval 200
       auto-save-timeout 20)
 
@@ -89,13 +119,178 @@
   (unless (file-exists-p auto-save-dir)
     (make-directory auto-save-dir))
   (setq auto-save-file-name-transforms `((".*" ,auto-save-dir t)))
+  (setq auto-save-list-file-prefix auto-save-dir)
   (setq tramp-auto-save-directory auto-save-dir))
 
-(setq delete-by-moving-to-trash t)
+(setq auto-save-include-big-deletions t)
+
+(setq revert-without-query (list ".")
+      auto-revert-stop-on-user-input nil
+      auto-revert-verbose t)
+
+(setq global-auto-revert-non-file-buffers t)
+
+(setq switch-to-buffer-obey-display-actions t)
+
+(setq uniquify-buffer-name-style 'forward)
+
+(setq comint-prompt-read-only t)
+(setq comint-buffer-maximum-size 2048)
+
+(setq confirm-nonexistent-file-or-buffer nil)
+
+(setq vc-git-print-log-follow t)
+(setq vc-make-backup-files nil)
+
+(setq recentf-max-saved-items 300)
+(setq recentf-max-menu-items 15)
+(setq recentf-auto-cleanup 'never)
+(defun minimal-emacs--cleanup-hook ()
+  "Run `recentf-cleanup' if `recentf' is loaded and `recentf-mode' is enabled."
+  (when (and (featurep 'recentf)
+             recentf-mode
+             (fboundp 'recentf-cleanup))
+    (recentf-cleanup)))
+(add-hook 'kill-emacs-hook #'minimal-emacs--cleanup-hook)
+
+(setq recentf-exclude (list "^/\\(?:ssh\\|su\\|sudo\\)?:"))
+
+(setq save-place-file (expand-file-name "saveplace" user-emacs-directory))
+(setq save-place-limit 600)
+
+(setq history-length 300)
+(setq savehist-save-minibuffer-history t)
+
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+(setq window-resize-pixelwise nil)
+
+(setq resize-mini-windows 'grow-only)
+
+(setq scroll-error-top-bottom t)
+
+(setq scroll-preserve-screen-position t)
+
+(setq scroll-conservatively 10)
+
+(setq scroll-step 1)
+
+(setq auto-window-vscroll nil)
+
+(setq scroll-margin 0)
+
+(setq hscroll-margin 2
+      hscroll-step 1)
+
+(setq pixel-scroll-precision-use-momentum nil)
+(pixel-scroll-precision-mode 1)
+
+(setq mouse-yank-at-point nil)
+
+(setq mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
+      mouse-wheel-scroll-amount-horizontal 2)
+
+(blink-cursor-mode -1)
+
+(setq blink-matching-paren nil)
+
+(setq x-stretch-cursor nil)
+
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+
+(setq visible-bell nil)
+(setq ring-bell-function #'ignore)
+
+(setq delete-pair-blink-delay 0.03)
+
+(setq-default left-fringe-width  8)
+(setq-default right-fringe-width 8)
+
+(setq-default indicate-buffer-boundaries nil)
+(setq-default indicate-empty-lines nil)
+
+(setq-default word-wrap t)
+
+(setq-default truncate-lines t)
+
+(setq truncate-partial-width-windows nil)
+
+(setq-default indent-tabs-mode nil
+              tab-width 4)
+
+(setq-default tab-always-indent nil)
+
+(setq read-extended-command-predicate #'command-completion-default-include-p)
+
+(setq comment-multi-line t)
+
+(setq-default fill-column 80)
+
+(setq sentence-end-double-space nil)
+(add-hook 'before-save-hook #'delete-trailing-whitespace)
+
+(setq require-final-newline t)
+
+(setq kill-do-not-save-duplicates t)
+
+(setq comment-empty-lines t)
+
+(setq lazy-highlight-initial-delay 0)
+
+(setq display-time-default-load-average nil)
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+(setq python-indent-guess-indent-offset-verbose nil)
+
+(setq sh-indent-after-continuation 'always)
+
+(setq dired-free-space nil
+      dired-dwim-target t
+      dired-deletion-confirmer 'y-or-n-p
+      dired-filter-verbose nil
+      dired-recursive-deletes 'top
+      dired-recursive-copies  'always
+      dired-create-destination-dirs 'ask
+      dired-auto-revert-buffer #'dired-buffer-stale-p
+      image-dired-thumb-size 150)
+
+(setq dired-clean-confirm-killing-deleted-buffers nil)
+
+(setq dired-omit-verbose nil)
+(setq dired-omit-files (concat "\\`[.]?#\\|\\`[.][.]?\\'"
+                               "\\|\\(?:\\.js\\)?\\.meta\\'"
+                               "\\|\\.\\(?:elc|a\\|o\\|pyc\\|pyo\\|swp\\|class\\)\\'"
+                               "\\|^\\.DS_Store\\'"
+                               "\\|^\\.\\(?:svn\\|git\\)\\'"
+                               "\\|^\\.ccls-cache\\'"
+                               "\\|^__pycache__\\'"
+                               "\\|^\\.project\\(?:ile\\)?\\'"
+                               "\\|^flycheck_.*"
+                               "\\|^flymake_.*"))
+
+(setq ls-lisp-verbosity nil)
+(setq ls-lisp-dirs-first t)
+
+(setq global-text-scale-adjust-resizes-frames nil)
+
+(setq treesit-font-lock-level 4)
+
+(set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 240 :weight 'medium)
+
+(setq ediff-window-setup-function #'ediff-setup-windows-plain
+      ediff-split-window-function #'split-window-horizontally)
+
+(setq apropos-do-all t)
+
+(setq help-enable-completion-autoload nil)
+(setq help-enable-autoload nil)
+(setq help-enable-symbol-autoload nil)
 
 (prefer-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(set-language-environment "English")
 (set-terminal-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (setq ispell-dictionary "en_US")
@@ -113,30 +308,42 @@
 (defun paste-to-osx (text &optional push)
   (let ((process-connection-type nil))
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
+  (process-send-string proc text)
+  (process-send-eof proc))))
 (when (and (not (display-graphic-p))
 	   (eq system-type 'darwin))
   (setq interprogram-cut-function 'paste-to-osx)
   (setq interprogram-paste-function 'copy-from-osx))
 
-(global-auto-revert-mode t)
+(add-hook 'after-init-hook #'global-auto-revert-mode)
+(add-hook 'after-init-hook #'recentf-mode)
+(add-hook 'after-init-hook #'savehist-mode)
+(add-hook 'after-init-hook #'save-place-mode)
 
-(setq sentence-end-double-space nil)
+(use-package compile-angel
+  :demand t
+  :config
+  (compile-angel-on-load-mode)
+  (add-hook 'emacs-lisp-mode-hook #'compile-angel-on-save-local-mode))
 
-(add-hook 'before-save-hook #'delete-trailing-whitespace)
+(use-package which-key
+  :config
+  (which-key-mode))
 
-(setq-default indent-tabs-mode nil)
-(setq tab-always-indent 'complete)
+(use-package uniquify
+  :ensure nil
+  :custom
+  (uniquify-buffer-name-style 'reverse)
+  (uniquify-separator "•")
+  (uniquify-after-kill-buffer-p t)
+  (uniquify-ignore-buffers-re "^\\*"))
 
-(global-hl-line-mode 1)
-
-(recentf-mode 1)
-(setq recentf-auto-cleanup 'never)
-(savehist-mode 1)
-(save-place-mode 1)
-(winner-mode 1)
-(xterm-mouse-mode 1)
+(use-package vterm
+  :defer t
+  :commands vterm
+  :config
+  ;; Speed up vterm
+  (setq vterm-timer-delay 0.01))
 
 (unload-feature 'eldoc t)
 (setq custom-delayed-init-variables '())
@@ -160,42 +367,16 @@
   :ensure (:wait t)
   :defer t)
 
-(use-package flymake
-  :config
-  (flymake-mode))
+(setq eglot-sync-connect 1
+      eglot-autoshutdown t)
+(setq eglot-extend-to-xref t)
+(setq jsonrpc-event-hook nil)
+(setq eglot-events-buffer-size 0)
+(setq eglot-report-progress nil)
 
-(use-package eglot
-  :defer t
-  :config
-  (add-hook 'eglot-server-initialized-hook #'flymake-mode))
+(setq eglot-events-buffer-config '(:size 0 :format full))
 
-(use-package treesit-auto
-  :config
-  (global-treesit-auto-mode))
-
-(use-package vterm)
-
-(setq tramp-terminal-type "tramp")
-
-(use-package transient
-  :defer t)
-
-(use-package magit
-  :defer t
-  :custom
-  (magit-diff-refine-hunk 'all)
-  :config
-  (transient-bind-q-to-quit))
-
-(use-package forge
-  :after magit)
-
-(setq dired-mouse-drag-files t)
-(setq dired-listing-switches "-alh")
-(setq dired-kill-when-opening-new-dired-buffer t)
-
-(setq treesit-font-lock-level 4)
-
-(set-face-attribute 'default nil :family "RobotoMono Nerd Font" :height 240 :weight 'medium)
-
-(use-package which-key)
+(setq flymake-fringe-indicator-position 'left-fringe)
+(setq flymake-show-diagnostics-at-end-of-line nil)
+(setq flymake-suppress-zero-counters t)
+(setq flymake-wrap-around nil)
